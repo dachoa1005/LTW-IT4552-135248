c349e83acc381462764e3b6e06c5d35e2210053e
dachoa1052000 <dachoa1052000@gmail.com>
2022-11-01 18:37:43 +0700

    add files

---
+++ Lab-02/expression.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <title>Expression Example</title>
+    
+    <?php
+        $gr1 = 50;
+        $gr2 = 100;
+        $gr3 = 75;
+        $aver = ($gr1 + $gr2 + $gr3) / 3;
+        echo "The average of $gr1, $gr2, and $gr3 is $aver";
+    ?>
+</body>
+</html>
